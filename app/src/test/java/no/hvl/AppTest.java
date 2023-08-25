73c183a6b55ae94daf8980a6532adbf14017c405
Beate Skogvik <beate.skogvik@student.uib.no>
2023-08-25 14:37:37 +0200

    added some tests

--- app/src/test/java/no/hvl/AppTest.java
+++ app/src/test/java/no/hvl/AppTest.java
@@ -6,9 +6,30 @@ package no.hvl;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+
+    @Test
+    public void testConvertFromUnitToMeters() {
+        assertEquals(0.0254, App.convertFromUnitToMeters(1, "in"), 0.001);
+        assertEquals(0.3048, App.convertFromUnitToMeters(1, "ft"), 0.001);
+        assertEquals(1609.344, App.convertFromUnitToMeters(1, "mi"), 0.001);
+        assertEquals(1, App.convertFromUnitToMeters(1, "m"));
+        assertEquals(Double.NaN, App.convertFromUnitToMeters(1, "invalid"));
     }
+
+    @Test
+    public void testConvertToUnitFromMeters() {
+        assertEquals(39.37007874015748, App.convertToUnitFromMeters(1, "in"), 0.0000001);
+        assertEquals(3.280839895013123, App.convertToUnitFromMeters(1, "ft"), 0.0000001);
+        assertEquals(0.0006213, App.convertToUnitFromMeters(1, "mi"), 0.0000001);
+        assertEquals(1, App.convertToUnitFromMeters(1, "m"));
+        assertEquals(Double.NaN, App.convertToUnitFromMeters(1, "invalid"));
+    }
+
+
 }
