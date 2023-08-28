0550884edcbc713ced551a529691a54140a45b90
Beate Skogvik <beate.skogvik@student.uib.no>
2023-08-28 09:58:08 +0200

    remove double imports

--- app/src/test/java/no/hvl/AppTest.java
+++ app/src/test/java/no/hvl/AppTest.java
@@ -5,10 +5,7 @@ package no.hvl;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 public class AppTest {
 
